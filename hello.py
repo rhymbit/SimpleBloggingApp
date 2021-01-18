71c1aba3a21134479dd1803b5648c7c78f1fdfb6
prateek332 <prateekn332@outlook.com>
2021-01-18 22:23:49 +0530

    Used `flask_moment` to include time stuff in the webpages `Moment.js` included in template `base.html`. Learned how to include JavaScripts in templates with `{% block scripts %}`

--- hello.py
+++ hello.py
@@ -1,16 +1,20 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+
+from datetime import datetime
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', current_time=datetime.utcnow())
 
 @app.route("/user/<name>")
 def user(name):
-    return render_template('user.html',name=name)
+    return render_template('user.html', name=name)
 
 @app.errorhandler(404)
 def page_not_found(e):
