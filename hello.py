cfe0d46095507c6a317a428ecc2fa2a18c72eccc
prateek332 <prateekn332@outlook.com>
2021-01-19 00:04:32 +0530

    Made User Form using `flask_wtf` and `wtforms` modules Lot learned here, basic user form created which takes in a username and greets user.`index.html` was modified to add form stuff. Learned about `FlaskForm` class to create forms.

--- hello.py
+++ hello.py
@@ -1,16 +1,30 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 from datetime import datetime
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string'
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
-@app.route('/')
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html', current_time=datetime.utcnow())
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template('index.html', form=form, name=name, current_time=datetime.utcnow())
 
 @app.route("/user/<name>")
 def user(name):
@@ -22,7 +36,7 @@ def page_not_found(e):
 
 @app.errorhandler(500)
 def internal_server_error(e):
-    return render_template('500.htm'), 500
+    return render_template('500.html'), 500
 
 if __name__ == '__main__':
     app.run()
