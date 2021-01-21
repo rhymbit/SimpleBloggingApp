dc8ede87eec5f07078c414b50ceb9fbd36914847
prateek332 <prateekn332@outlook.com>
2021-01-21 12:12:43 +0530

    Used Redirect and user Sessions Learned about how to use `redirect`, `session` and `url_for` from flask module. Made changes in `def index():` using them.

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_wtf import FlaskForm
@@ -19,12 +19,11 @@ class NameForm(FlaskForm):
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    name = None
     form = NameForm()
     if form.validate_on_submit():
-        name = form.name.data
-        form.name.data = ''
-    return render_template('index.html', form=form, name=name, current_time=datetime.utcnow())
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'), current_time=datetime.utcnow())
 
 @app.route("/user/<name>")
 def user(name):
@@ -39,4 +38,4 @@ def internal_server_error(e):
     return render_template('500.html'), 500
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
