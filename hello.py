3152dfe735c83e93b1ec110b742d81102573db92
prateek332 <prateekn332@outlook.com>
2021-01-23 23:22:03 +0530

    Created Flask_SQLAlchemy Database, Learned its Basics Used `flask_sqlalchemy`, create tables `roles` and `users` using sqlite. Learned `flask shell` and using database from it.

--- hello.py
+++ hello.py
@@ -2,19 +2,51 @@ from flask import Flask, render_template, session, redirect, url_for
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_wtf import FlaskForm
+from flask_sqlalchemy import SQLAlchemy
+
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 
+from pathlib import Path
+
 from datetime import datetime
 
+basedir = Path(__file__).parent
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + str(Path.joinpath(basedir,'data.sqlite'))
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'hard to guess string'
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+db = SQLAlchemy(app)
 
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
     submit = SubmitField('Submit')
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+
+    users = db.relationship('User', backref='role', lazy='dynamic')
+
+    def __repr__(self):
+        return f'<Role {self.name!r}>'
+
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+    def __repr__(self):
+        return f'<User {self.username!r}>'
+    
 
 
 @app.route('/', methods=['GET', 'POST'])
