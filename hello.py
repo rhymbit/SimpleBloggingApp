b200610cd741b457d62550bcecc7445622de4926
prateek332 <prateekn332@outlook.com>
2021-01-27 21:40:38 +0530

    Learned Database Migration with Flask Don't know what I did, just followed the guide, pretty sure I'll forget about it in some time.

--- hello.py
+++ hello.py
@@ -1,15 +1,17 @@
 from flask import Flask, render_template, session, redirect, url_for
+
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_wtf import FlaskForm
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 
 from pathlib import Path
-
 from datetime import datetime
+
 
 basedir = Path(__file__).parent
 
@@ -20,6 +22,7 @@ app.config['SECRET_KEY'] = 'hard to guess string'
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
