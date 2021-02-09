7e5906d98d286982be36bc1ecdfa46a7143cfbe3
prateek332 <prateekn332@outlook.com>
2021-02-09 18:21:41 +0530

    Added pagination support for User's profile pages.

--- app/main/views.py
+++ app/main/views.py
@@ -33,8 +33,12 @@ def index():
 @main.route('/user/<username>')
 def user(username):
     user = User.query.filter_by(username=username).first_or_404()
-    posts = user.posts.order_by(Post.timestamp.desc()).all()
-    return render_template('user.html', user=user, posts=posts)
+    page = request.args.get('page', 1, type=int)
+    pagination = user.posts.order_by(Post.timestamp.desc()).paginate(
+        page, per_page=current_app.config['FLASKY_POSTS_PER_PAGE'],
+        error_out=False)
+    posts = pagination.items
+    return render_template('user.html', user=user, posts=posts, pagination=pagination)
 
 @main.route('/edit-profile', methods=['GET','POST'])
 @login_required
